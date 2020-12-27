ffde75542d140de7c63376d1b3ea5f9a242b3a51
Xephyrus <rtx2080titan@163.com>
2020-12-28 00:19:46 +0800

    about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,6 +3,9 @@
     <main>
       <h1>This is an about page</h1>
       <time-table></time-table>
+      <div class="text">
+        <p>这是一个前端项目， 使用vue + elementui + 自定义组件搭建， 以css 和 scss文件定义公共样式， 使用Primsmjs美化页面的代码文本，使用element UI二次封装自己的组件。</p>
+      </div>
     </main>
   </div>
 </template>
@@ -24,6 +27,12 @@ main {
     font-size: 2em;
     color: #fff;
     margin-bottom: 1em;
+  }
+
+  .text {
+    margin-top: 3em;
+    background: #ffffff;
+    padding: 1em;
   }
 }
 </style>
